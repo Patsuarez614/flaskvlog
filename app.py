0b18f9fcfee2394e930ebf49879888d8df655d2c
Patsuarez614 <43017935+Patsuarez614@users.noreply.github.com>
2019-05-27 14:22:03 -0400

    add pages

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "<h1>Hello Patrick!<h1>"
+def home():
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return "<h1>About Patrick!<h1>"
+    return render_template('about.html')
